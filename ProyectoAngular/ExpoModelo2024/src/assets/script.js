dc991884d9f8a4e4af6743998ce68f7ab5baae58
a <a>
2023-11-10 09:36:07 -0300

    tutorial de external page no anda

--- ProyectoAngular/ExpoModelo2024/src/assets/script.js
+++ ProyectoAngular/ExpoModelo2024/src/assets/script.js
@@ -1,46 +1,9 @@
-console.log("aa");
-function imageZoom(imgID, resultID){
-    var img, lens, result, cx, cy;
-    img = document.getElementById("DIV");
-    lens.setAttribute("class", "img-zoom-lens");
-
-    img.parentElement.insertBefore(lens, img);
-
-    cx=result.offsetWidth / lens.offsetWidth;
-    cy=result.offsetHeight / lens.offsetHeight;
-
-    result.style.backgroundImage="url(`" + img.src + "`)";
-    result.style.backgroundSize=(img.width * cx) + "px " + (img.height * cy) + "px";
-    
-    lens.addEventLidtener("mousemove", moveLens);
-    img.addEventLidtener("mousemove", moveLens);
-
-    lens.addEventLidtener("touchmove", moveLens);
-    img.addEventLidtener("touchmove", moveLens);
-
-    function moveLens(e){
-
-        var pos, x, y;
-        pos=getCursorPos(e);
-        x = pos.x - (lens.offsetWidth / 2);
-        y = pos.y - (lens.offsetHeight / 2);
-        if(x > img.width - lens.offsetWidth) { x = img.width - lens.offsetWidth}
-        if(x < 0) { x = 0;}
-        if(y > img.height - lens.offsetHeight) { x = img.height - lens.offsetHeight}
-        if(y < 0) { x = 0;}
-        lens.style.left = x + "px";
-        lens.style.top = y + "px";
-        result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
-    }
-
-    function getCursorPos(e){
-        var a, x = 0, y = 0;
-        e = e || window.event;
-        a = img.getBoundingClientRect();
-        x = e.pageX -a.left;
-        y = e.pageY - a.top;
-        x = x - window.pageXOffset;
-        y = y - window.pageYOffset;
-        return { x : x, y : y};
-    }
+document.querySelectorAll('.image-container img').forEach(image=>{
+    image.onclick = () =>{
+        document.querySelector('.popup-image').style.display = "block";
+        document.querySelector('.popup-image img').src = image.getAttribute('src');
+    }    
+});
+document.querySelector('.popup-image span').onclick = () =>{
+    document.querySelector('.popup-image').style.display = "none";
 }
